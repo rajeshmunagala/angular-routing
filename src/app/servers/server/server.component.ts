6cd07c003878d7517f665dd9df54b342d6b90aa4
Rajesh Munagala <rajeshmunagala3@gmail.com>
2019-08-13 20:13:39 +0530

    first commit

---
+++ src/app/servers/server/server.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+
+import { ServersService } from '../servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { relative } from 'path';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  server: {id: number, name: string, status: string};
+
+  constructor(private serversService: ServersService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.server = this.serversService.getServer(1);
+  }
+  onReload(){
+    //this.router.navigate(['servers'], {relativeTo: this.route});
+  }
+
+}
